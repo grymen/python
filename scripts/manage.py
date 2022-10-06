f2b83a38d7a98da15dcb8085b37a6c891356ed2a
Grymen <grymen@bk.ru>
2022-10-06 20:17:19 +0300

    Add lesson 2

--- scripts/manage.py
+++ scripts/manage.py
@@ -1,3 +1,13 @@
 # This program prints Hello, world!
 
-print('Hello, world!')
+name = input("What is your name? ")
+a = f'Hello {name}!'
+print(a)
+
+age = int(input("How old are you? "))
+if age > 18:
+    print('You are already an adult')
+else:
+    print('Haha, it\'s too early for you beer')
+b = f'Age: {age}'
+print(b)
