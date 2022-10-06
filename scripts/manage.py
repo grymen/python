26c49c8527aa482ebe6ed95eb0d92cd0ee15b795
Grymen <grymen@bk.ru>
2022-10-06 20:28:57 +0300

    Add math operation

--- scripts/manage.py
+++ scripts/manage.py
@@ -1,5 +1,4 @@
-# This program prints Hello, world!
-
+# part about name
 name = input("What is your name? ")
 a = f'Hello {name}!'
 print(a)
@@ -11,3 +10,15 @@ else:
     print('Haha, it\'s too early for you beer')
 b = f'Age: {age}'
 print(b)
+
+# Mathematical operation
+temp = age + 5
+print(f'age + 5 = {temp}')
+temp = age * 2
+print(f'age * 2 = {temp}')
+temp = age / 3
+print(f'age / 3 = {temp}')
+temp += age 
+print(f'age += {temp}')
+temp = age // 5
+print(f'age // 5 = {temp}')
